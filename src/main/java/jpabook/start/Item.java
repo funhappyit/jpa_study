577870285d0b17445274c0636f4705608eef7b15
“funhappyit” <fun.happyit@gmail.com>
2024-03-31 16:37:56 +0900

    2024-03-31 7장 상속 관계 매핑 실전 예제

--- src/main/java/jpabook/start/Item.java
+++ src/main/java/jpabook/start/Item.java
@@ -3,14 +3,12 @@ package jpabook.start;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 
 @Entity
-public class Item {
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="DTYPE")
+public abstract class Item {
     @Id
     @GeneratedValue
     @Column(name="ITEM_ID")
--- src/main/java/jpabook/start/OrderStatus.java
+++ src/main/java/jpabook/start/OrderStatus.java
@@ -2,4 +2,6 @@ package jpabook.start;
 
 public enum OrderStatus {
     ORDER,CANCEL
+
+
 }
