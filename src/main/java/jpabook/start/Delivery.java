e3835f7a10d7801cec72ec1b3f47a6fb73cf61d9
“funhappyit” <fun.happyit@gmail.com>
2024-04-03 20:43:20 +0900

    2024-04-03 9장 실전예제

--- src/main/java/jpabook/start/Delivery.java
+++ src/main/java/jpabook/start/Delivery.java
@@ -1,12 +1,6 @@
 package jpabook.start;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Entity
 public class Delivery {
@@ -18,9 +12,8 @@ public class Delivery {
 	@OneToOne(mappedBy = "delivery")
 	private Order order;
 
-	private String city;
-	private String street;
-	private String zipcode;
+	@Embedded
+	private Address address;
 
 	@Enumerated(EnumType.STRING)
 	private DeliveryStatus status;
@@ -39,30 +32,6 @@ public class Delivery {
 
 	public void setOrder(Order order) {
 		this.order = order;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getStreet() {
-		return street;
-	}
-
-	public void setStreet(String street) {
-		this.street = street;
-	}
-
-	public String getZipcode() {
-		return zipcode;
-	}
-
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
 	}
 
 	public DeliveryStatus getStatus() {
--- src/main/java/jpabook/start/Member.java
+++ src/main/java/jpabook/start/Member.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Member extends BaseEntity{
+public class Member{
 
     @Id @GeneratedValue
     @Column(name="MEMBER_ID")
@@ -12,12 +12,19 @@ public class Member extends BaseEntity{
 
     private String name;
 
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
     public Long getId() {
         return id;
@@ -33,30 +40,6 @@ public class Member extends BaseEntity{
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
     }
 
     public List<Order> getOrders() {
